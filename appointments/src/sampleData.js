9451460f63ebdda536018a1359baeeae46f62cce
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 14:14:42 +0000

    Add faker to provide better sample data

--- appointments/src/sampleData.js
+++ appointments/src/sampleData.js
@@ -1,15 +1,52 @@
-const today = new Date();
+import { name, phone, lorem } from 'faker';
 
+Array.prototype.unique = function() {
+  return this.filter(function(value, index, self) {
+    return self.indexOf(value) === index;
+  });
+};
+
+Array.prototype.pickRandom = function() {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+const today = new Date();
 const at = hours => today.setHours(hours, 0);
 
+const stylists = [0, 1, 2, 3, 4, 5, 6]
+  .map(() => name.firstName())
+  .unique();
+
+const services = [
+  'Cut',
+  'Blow-dry',
+  'Cut & color',
+  'Beard trim',
+  'Cut & beard trim',
+  'Extensions'
+];
+
+const generateFakeCustomer = () => ({
+  firstName: name.firstName(),
+  lastName: name.lastName(),
+  phoneNumber: phone.phoneNumberFormat(1)
+});
+
+const generateFakeAppointment = () => ({
+  customer: generateFakeCustomer(),
+  stylist: stylists.pickRandom(),
+  service: services.pickRandom(),
+  notes: lorem.paragraph()
+});
+
 export const sampleAppointments = [
-  { startsAt: at(9), customer: { firstName: 'Charlie' } },
-  { startsAt: at(10), customer: { firstName: 'Frankie' } },
-  { startsAt: at(11), customer: { firstName: 'Casey' } },
-  { startsAt: at(12), customer: { firstName: 'Ashley' } },
-  { startsAt: at(13), customer: { firstName: 'Jordan' } },
-  { startsAt: at(14), customer: { firstName: 'Jay' } },
-  { startsAt: at(15), customer: { firstName: 'Alex' } },
-  { startsAt: at(16), customer: { firstName: 'Jules' } },
-  { startsAt: at(17), customer: { firstName: 'Stevie' } }
+  { startsAt: at(9), ...generateFakeAppointment() },
+  { startsAt: at(10), ...generateFakeAppointment() },
+  { startsAt: at(11), ...generateFakeAppointment() },
+  { startsAt: at(12), ...generateFakeAppointment() },
+  { startsAt: at(13), ...generateFakeAppointment() },
+  { startsAt: at(14), ...generateFakeAppointment() },
+  { startsAt: at(15), ...generateFakeAppointment() },
+  { startsAt: at(16), ...generateFakeAppointment() },
+  { startsAt: at(17), ...generateFakeAppointment() }
 ];

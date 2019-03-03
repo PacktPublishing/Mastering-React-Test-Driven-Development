513424c37670effd15da74946d0e8b187a6a9ebf
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 17:59:19 +0000

    Add first name text field

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,3 +1,14 @@
 import React from 'react';
 
-export const CustomerForm = () => <form id="customer" />;
+export const CustomerForm = ({ firstName }) => (
+  <form id="customer">
+    <label htmlFor="firstName">First name</label>
+    <input
+      type="text"
+      name="firstName"
+      id="firstName"
+      value={firstName}
+      readOnly
+    />
+  </form>
+);
--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -10,9 +10,41 @@ describe('CustomerForm', () => {
   });
 
   const form = id => container.querySelector(`form[id="${id}"]`);
+  const firstNameField = () => form('customer').elements.firstName;
+  const labelFor = formElement =>
+    container.querySelector(`label[for="${formElement}"]`);
 
   it('renders a form', () => {
     render(<CustomerForm />);
     expect(form('customer')).not.toBeNull();
+  });
+
+  const expectToBeInputFieldOfTypeText = formElement => {
+    expect(formElement).not.toBeNull();
+    expect(formElement.tagName).toEqual('INPUT');
+    expect(formElement.type).toEqual('text');
+  };
+
+  it('renders the first name field as a text box', () => {
+    render(<CustomerForm />);
+    expectToBeInputFieldOfTypeText(firstNameField());
+  });
+
+  it('includes the existing value for the first name', () => {
+    render(<CustomerForm firstName="Ashley" />);
+    expect(firstNameField().value).toEqual('Ashley');
+  });
+
+  it('renders a label for the first name field', () => {
+    render(<CustomerForm />);
+    expect(labelFor('firstName')).not.toBeNull();
+    expect(labelFor('firstName').textContent).toEqual(
+      'First name'
+    );
+  });
+
+  it('assigns an id that matches the label id to the first name field', () => {
+    render(<CustomerForm />);
+    expect(firstNameField().id).toEqual('firstName');
   });
 });

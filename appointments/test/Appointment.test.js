f6863d31958d950cfb1cf365d206d41f131536d5
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 09:17:38 +0000

    Dry up tests

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -3,27 +3,25 @@ import ReactDOM from 'react-dom';
 import { Appointment } from '../src/Appointment';
 
 describe('Appointment', () => {
+  let container;
+  let customer;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  const render = component =>
+    ReactDOM.render(component, container);
+
   it('renders the customer first name', () => {
-    const customer = { firstName: 'Ashley' };
-    const container = document.createElement('div');
-
-    ReactDOM.render(
-      <Appointment customer={customer} />,
-      container
-    );
-
+    customer = { firstName: 'Ashley' };
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Ashley');
   });
 
   it('renders another customer first name', () => {
-    const customer = { firstName: 'Jordan' };
-    const container = document.createElement('div');
-
-    ReactDOM.render(
-      <Appointment customer={customer} />,
-      container
-    );
-
+    customer = { firstName: 'Jordan' };
+    render(<Appointment customer={customer} />);
     expect(container.textContent).toMatch('Jordan');
   });
 });

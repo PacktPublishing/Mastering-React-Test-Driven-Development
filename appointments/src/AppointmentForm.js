e3aa12eaadd6b599cd8964ccafc6eabd166bd46d
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 22:35:17 +0000

    Add service dropdown options and defaultProps

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-export const AppointmentForm = () => (
+export const AppointmentForm = ({ selectableServices }) => (
   <form id="appointment">
-    <select name="service" />
+    <select name="service">
+      <option />
+      {selectableServices.map(s => (
+        <option key={s}>{s}</option>
+      ))}
+    </select>
   </form>
 );
+
+AppointmentForm.defaultProps = {
+  selectableServices: [
+    'Cut',
+    'Blow-dry',
+    'Cut & color',
+    'Beard trim',
+    'Cut & beard trim',
+    'Extensions'
+  ]
+};
--- appointments/test/AppointmentForm.test.js
+++ appointments/test/AppointmentForm.test.js
@@ -23,5 +23,28 @@ describe('AppointmentForm', () => {
       expect(field('service')).not.toBeNull();
       expect(field('service').tagName).toEqual('SELECT');
     });
+
+    it('initially has a blank value chosen', () => {
+      render(<AppointmentForm />);
+      const firstNode = field('service').childNodes[0];
+      expect(firstNode.value).toEqual('');
+      expect(firstNode.selected).toBeTruthy();
+    });
+
+    it('lists all salon services', () => {
+      const selectableServices = ['Cut', 'Blow-dry'];
+
+      render(
+        <AppointmentForm selectableServices={selectableServices} />
+      );
+
+      const optionNodes = Array.from(field('service').childNodes);
+      const renderedServices = optionNodes.map(
+        node => node.textContent
+      );
+      expect(renderedServices).toEqual(
+        expect.arrayContaining(selectableServices)
+      );
+    });
   });
 });

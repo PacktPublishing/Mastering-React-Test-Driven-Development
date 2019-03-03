6d9288895df39e5c7afd81a78d83a21e9b1f9845
Daniel Irvine <daniel@danielirvine.com>
2019-03-03 17:34:58 +0000

    Add initial CustomerForm

---
+++ appointments/test/CustomerForm.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createContainer } from './domManipulators';
+import { CustomerForm } from '../src/CustomerForm';
+
+describe('CustomerForm', () => {
+  let render, container;
+
+  beforeEach(() => {
+    ({ render, container } = createContainer());
+  });
+
+  const form = id => container.querySelector(`form[id="${id}"]`);
+
+  it('renders a form', () => {
+    render(<CustomerForm />);
+    expect(form('customer')).not.toBeNull();
+  });
+});

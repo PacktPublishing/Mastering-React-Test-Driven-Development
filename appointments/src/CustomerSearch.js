4fa15debc4464eb5e3748597a80575e26eeaf150
Daniel Irvine <daniel@danielirvine.com>
2019-03-26 07:49:35 +0000

    Display customers in table

--- appointments/src/CustomerSearch.js
+++ appointments/src/CustomerSearch.js
@@ -1,3 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export const CustomerSearch = () => null;
+const CustomerRow = ({ customer }) => (
+  <tr>
+    <td>{customer.firstName}</td>
+    <td>{customer.lastName}</td>
+    <td>{customer.phoneNumber}</td>
+    <td />
+  </tr>
+);
+
+export const CustomerSearch = () => {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await window.fetch('/customers', {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' }
+      });
+      setCustomers(await result.json());
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Phone number</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {customers.map(customer => (
+          <CustomerRow customer={customer} key={customer.id} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
--- appointments/test/CustomerSearch.test.js
+++ appointments/test/CustomerSearch.test.js
@@ -4,6 +4,15 @@ import { createContainer, withEvent } from './domManipulators';
 import { CustomerSearch } from '../src/CustomerSearch';
 import { fetchResponseOk } from './spyHelpers';
 
+const oneCustomer = [
+  { id: 1, firstName: 'A', lastName: 'B', phoneNumber: '1' }
+];
+
+const twoCustomers = [
+  { id: 1, firstName: 'A', lastName: 'B', phoneNumber: '1' },
+  { id: 2, firstName: 'C', lastName: 'D', phoneNumber: '2' }
+];
+
 describe('CustomerSearch', () => {
   let renderAndWait, container, element, elements;
 
@@ -17,5 +26,41 @@ describe('CustomerSearch', () => {
     jest
       .spyOn(window, 'fetch')
       .mockReturnValue(fetchResponseOk([]));
+  });
+
+  it('renders a table with four headings', async () => {
+    await renderAndWait(<CustomerSearch />);
+    const headings = elements('table th');
+    expect(headings.map(h => h.textContent)).toEqual([
+      'First name',
+      'Last name',
+      'Phone number',
+      'Actions'
+    ]);
+  });
+
+  it('fetches all customer data when component mounts', async () => {
+    await renderAndWait(<CustomerSearch />);
+    expect(window.fetch).toHaveBeenCalledWith('/customers', {
+      method: 'GET',
+      credentials: 'same-origin',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  });
+
+  it('renders all customer data in a table row', async () => {
+    window.fetch.mockReturnValue(fetchResponseOk(oneCustomer));
+    await renderAndWait(<CustomerSearch />);
+    const columns = elements('table > tbody > tr > td');
+    expect(columns[0].textContent).toEqual('A');
+    expect(columns[1].textContent).toEqual('B');
+    expect(columns[2].textContent).toEqual('1');
+  });
+
+  it('renders multiple customer rows', async () => {
+    window.fetch.mockReturnValue(fetchResponseOk(twoCustomers));
+    await renderAndWait(<CustomerSearch />);
+    const rows = elements('table tbody tr');
+    expect(rows[1].childNodes[0].textContent).toEqual('C');
   });
 });

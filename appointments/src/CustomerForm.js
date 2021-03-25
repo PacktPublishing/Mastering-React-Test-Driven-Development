6b8701a5a779977d77110538524434d96989ece5
NBESS <nicholasdbess@gmail.com>
2021-03-25 13:34:39 -0400

    Add test to verify form saves first name on submit

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-export const CustomerForm = ({ firstName }) => (
-  <form id="customer">
-    <label htmlFor="firstName">First name</label>
-    <input
-      type="text"
-      name="firstName"
-      id="firstName"
-      value={firstName}
-      readOnly
-    />
-  </form>
-);
+export const CustomerForm = ({ firstName, onSubmit }) => {
+  const customer = { firstName };
+  return (
+    <form id="customer" onSubmit={() => onSubmit(customer)}>
+      <label htmlFor="firstName">First name</label>
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        value={firstName}
+        readOnly
+      />
+    </form>
+  );
+};
--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -1,6 +1,7 @@
 import React, { createElement } from 'react';
 import { createContainer } from './domManipulators';
 import { CustomerForm } from '../src/CustomerForm';
+import ReactTestUtils from 'react-dom/test-utils';
 
 describe('CustomerForm', () => {
   let render, container;
@@ -44,5 +45,18 @@ describe('CustomerForm', () => {
   it('assigns an id that matches the label id to the first name field', () => {
     render(<CustomerForm />);
     expect(firstNameField().id).toEqual('firstName');
+  });
+
+  it('saves existing first name when submitted', async () => {
+    expect.hasAssertions();
+    render(
+      <CustomerForm
+        firstName="Ashley"
+        onSubmit={({ firstName }) =>
+          expect(firstName).toEqual('Ashley')
+        }
+      />
+    );
+    await ReactTestUtils.Simulate.submit(form('customer'));
   });
 });

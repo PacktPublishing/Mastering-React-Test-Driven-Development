b7ada592309fc729ce33a8e87450c02092714012
NBESS <nicholasdbess@gmail.com>
2021-03-31 14:46:20 -0400

    Add for salon service select box

---
+++ appointments/test/AppointmentForm.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { createContainer } from './domManipulators';
+import { AppointmentForm } from '../src/AppointmentForm';
+
+describe('AppointmentForm', () => {
+  let render, container;
+
+  beforeEach(() => {
+    ({ render, container } = createContainer());
+  });
+
+  const form = (id) => container.querySelector(`form[id="${id}"]`);
+  const field = (name) => form('appointment').elements[name];
+
+  it('renders a form', () => {
+    render(<AppointmentForm />);
+    expect(form('appointment')).not.toBeNull();
+  });
+
+  describe('service field', () => {
+    it('renders as a select box', () => {
+      render(<AppointmentForm />);
+      expect(field('service')).not.toBeNull();
+      expect(field('service').tagName).toEqual('SELECT');
+    });
+
+    it('initially has a blank value chosen', () => {
+      render(<AppointmentForm />);
+      const firstNode = field('service').childNodes[0];
+      expect(firstNode.value).toEqual('');
+      expect(firstNode.selected).toBeTruthy();
+    });
+
+    it('lists all salon services', () => {
+      const selectableServices = ['Cut', 'Blow-dry'];
+      render(
+        <AppointmentForm selectableServices={selectableServices} />
+      );
+      const optionsNodes = Array.from(field('service').childNodes);
+      const renderedServices = optionsNodes.map(
+        (node) => node.textContent
+      );
+      expect(renderedServices).toEqual(
+        expect.arrayContaining(selectableServices)
+      );
+    });
+  });
+});

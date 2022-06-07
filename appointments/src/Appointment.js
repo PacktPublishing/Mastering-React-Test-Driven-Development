801e4a896f4f7463d5f00e6dd65c37fe25b36be0
lthemis <lukasz.tylke@gmail.com>
2022-06-07 20:35:07 +0200

    test: AppointmentsDayView

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+
+const appointmentTimeOfDay = startsAt => {
+  const [h, m] = new Date(startsAt).toTimeString().split(':');
+  return `${h}:${m}`;
+}
+
 export const Appointment = ({ customer }) => (
   <div>{customer.firstName}</div>
 );
+
+export const AppointmentsDayView = ({appointments}) => <div id="appointmentsDayView"><ol>{appointments.map((appointment)=>{
+  return <li key={appointment.startsAt}>
+    {appointmentTimeOfDay(appointment.startsAt)}
+  </li>
+})}</ol></div>;
+;
--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
+
 
 describe('Appointment', () => {
   let container;
@@ -25,3 +26,39 @@ describe('Appointment', () => {
     expect(container.textContent).toMatch('Jordan');
   });
 });
+
+describe('AppointmentDayView', () => {
+  const today = new Date();
+  const appointments = [
+    {startsAt: today.setHours(12,0)},
+    {startsAt: today.setHours(13,0)}
+  ]
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  const render = component => ReactDOM.render(component, container);
+  it('renders component with the right ID', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+    });
+
+  it('renders multiple appointments in an ol element',() => {
+    render(<AppointmentsDayView appointments={appointments} />)
+    expect(container.querySelector('ol')).not.toBeNull()
+    expect(container.querySelector('ol').children).toHaveLength(2)
+  })
+
+  it('renders each appointment in an li', () => {
+    render(<AppointmentsDayView appointments={appointments} />);
+    expect(container.querySelectorAll('li')).toHaveLength(2);
+    expect(
+      container.querySelectorAll('li')[0].textContent
+    ).toEqual('12:00');
+    expect(
+      container.querySelectorAll('li')[1].textContent
+    ).toEqual('13:00');
+});
+})
